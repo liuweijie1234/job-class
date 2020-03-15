31709a9e9e1ec69d5d599625082d71c7beb88487
刘伟杰 <v_awjliu@tencent.com>
2020-03-15 20:23:08 +0800

    1

--- joblwj/views.py
+++ joblwj/views.py
@@ -8,19 +8,20 @@ from blueking.component.shortcuts import get_client_by_request
 # 装饰器引入 from blueapps.account.decorators import login_exempt
 
 
+
 def tasks(request):
     taskses = SelectScript.objects.all()
     client = get_client_by_request(request)
     # client = get_client_by_user("admin")
     kwargs = {
-                "bk_app_code": "job-class",
-                "bk_app_secret": "ab784530-398f-4a75-819a-4fdf63dfddea",
-                "bk_token": "Delqj_TcSBLNmBy0yOTpht7Y_ZXdhsziSLo5Mg0fUbU",
-                "fields": [
-                    "bk_biz_id",
-                    "bk_biz_name"
-                ]
-            }
+        "bk_app_code": "job-class",
+        "bk_app_secret": "ab784530-398f-4a75-819a-4fdf63dfddea",
+        "bk_token": "Delqj_TcSBLNmBy0yOTpht7Y_ZXdhsziSLo5Mg0fUbU",
+        "fields": [
+            "bk_biz_id",
+            "bk_biz_name"
+        ]
+    }
     result = client.cc.search_business(kwargs)
     a = result['data']['info']
     b = []
@@ -28,10 +29,25 @@ def tasks(request):
         b.append(i['bk_biz_name'])
     return render(request, 'tasks.html', {"taskses": taskses, "business": b})
 
-
 def record(request):
-
-    return render(request, 'record.html')
+    taskses = SelectScript.objects.all()
+    # client = get_client_by_request(request)
+    client = get_client_by_user("admin")
+    kwargs = {
+        "bk_app_code": "job-class",
+        "bk_app_secret": "ab784530-398f-4a75-819a-4fdf63dfddea",
+        "bk_token": "Delqj_TcSBLNmBy0yOTpht7Y_ZXdhsziSLo5Mg0fUbU",
+        "fields": [
+            "bk_biz_id",
+            "bk_biz_name"
+        ]
+    }
+    result = client.cc.search_business(kwargs)
+    a = result['data']['info']
+    b = []
+    for i in a:
+        b.append(i['bk_biz_name'])
+    return render(request, 'record.html', {"taskses": taskses, "business": b})
 
 
 

49f2d7467bb42fc126c9c2f321207e32591711d3
TENCENT\v_awjliu <v_awjliu@tencent.com>
2020-04-01 20:42:42 +0800

    11

--- config/dev.py
+++ config/dev.py
@@ -20,7 +20,7 @@ BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 # BROKER_URL = 'redis://localhost:6379/0'
 
 DEBUG = True
-SITE_URL = os.getenv('BKPAAS_SUB_PATH', '/')
+
 # 本地开发数据库设置
 # USE FOLLOWING SQL TO CREATE THE DATABASE NAMED APP_CODE
 # SQL: CREATE DATABASE `framework_py` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; # noqa: E501
